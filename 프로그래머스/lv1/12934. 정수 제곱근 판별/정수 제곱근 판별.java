bd28da7e707da402f2d48f2a09977f547394c65b
wolveshowl <123453152+wolveshowl@users.noreply.github.com>
2023-07-04 09:41:41 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,19 +1,16 @@
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        long x = 0;
+        long x = 1;
 
-        while (true) {
+        while (n >= x * x) {
             if (n == x * x) {
                 answer = (x+1) * (x+1);
                 break;
             }
-            if (n < x * x) {
-                answer = -1;
-                break;
-            }
             x++;
         }
-        return answer;
+        
+        return answer = answer == 0 ? -1 : answer;
     }
 }
