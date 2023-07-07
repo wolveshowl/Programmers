0ec5782da8896b5ba568691b5539011aa61048ec
wolveshowl <123453152+wolveshowl@users.noreply.github.com>
2023-07-07 20:09:01 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.91 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,15 +1,7 @@
+import java.util.*;
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "";
-        int index = 0;
-        
-        for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")) {
-                index = i;
-                break;
-            }
-        }
-        answer = "김서방은 " + index + "에 있다";
-        return answer;
+        int index = Arrays.asList(seoul).indexOf("Kim");        
+        return "김서방은 "+ index + "에 있다";
     }
 }
