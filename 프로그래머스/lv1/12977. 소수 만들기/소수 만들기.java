1a639d73b8151c4027f371922c23e66d0595135d
wolveshowl <123453152+wolveshowl@users.noreply.github.com>
2023-08-02 17:01:56 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -4,10 +4,8 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int temp = 0;
-                    temp = nums[i] + nums[j] + nums[k];
-                    if (isPrimeNumber(temp)) {
+                for (int k = j + 1; k < nums.length; k++) {                                        
+                    if (isPrimeNumber(nums[i] + nums[j] + nums[k])) {
                         answer++;
                     }
                 }
