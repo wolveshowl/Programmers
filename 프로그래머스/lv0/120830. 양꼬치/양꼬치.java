ed39c8ca84a5850a55bdc2c75309302e7735feb8
wolveshowl <dipper25@naver.com>
2023-09-02 23:53:36 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        answer = (n * 12000) + (k * 2000);
+        int div = n / 10;
+        return answer = answer - (2000 * div);
+    }
+}
