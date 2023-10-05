089feebc725baf10471f7cc3e66f4bf585be7c08
wolveshowl <dipper25@naver.com>
2023-10-05 23:55:56 +0900

    [unrated] Title: 옹알이 (2), Time: 4.33 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.stream.Collectors;
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i=0; i<babbling.length; i++){
+            if(babbling[i].matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
