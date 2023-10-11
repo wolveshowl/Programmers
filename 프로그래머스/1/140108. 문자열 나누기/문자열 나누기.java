d69f741d25163b210ddb15dccf2d6fa18890bf76
wolveshowl <dipper25@naver.com>
2023-10-11 22:34:06 +0900

    [level 1] Title: 문자열 나누기, Time: 0.48 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        char c = s.charAt(0);
+        int idx = 0, jdx = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (idx == jdx) {
+                answer++;
+                c = s.charAt(i);
+            }
+
+            if (s.charAt(i) == c) idx++;
+            else jdx++;
+        }
+
+        return answer;
+    }
+}
